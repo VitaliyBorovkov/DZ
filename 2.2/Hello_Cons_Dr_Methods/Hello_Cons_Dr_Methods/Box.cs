c998741f81d8ad36ec563c3cc812a218bd6f1544
VitaliyBorovkov <97561291+VitaliyBorovkov@users.noreply.github.com>
2023-01-18 22:12:03 +0200

    Выполняю 2.2

--- 2.2/Hello_Cons_Dr_Methods/Hello_Cons_Dr_Methods/Box.cs
+++ 2.2/Hello_Cons_Dr_Methods/Hello_Cons_Dr_Methods/Box.cs
@@ -10,25 +10,22 @@ namespace Hello_Cons_Dr_Methods
     public class Box
     {
         //1.  Implement public  auto-implement properties for start position (point position)
+
         //auto-implement properties for width and height of the box
+
         //and auto-implement properties for a symbol of a given set of valid characters
         //(*, + ,.) to be used for the border 
         //and a message inside the box
 
-        public int positionX = 10;
-        public int positionY = 5;
+        public int positionX;
+        public int positionY;
 
-        public int height = 7;
-        public int width = 40;
+        public int height;
+        public int width;
 
         public char border;
-        public char space = ' ';
-        //public char star = '*';
-        //public char plus = '+';
-        //public char dot = '.';
-        //public char dies = '#';
-
-        public string message = " Message inside the box. ";
+        public char space = ' ';        
+        public string message;
 
         public void StartPosition(int positionX, int positionY)
         {
@@ -41,26 +38,32 @@ namespace Hello_Cons_Dr_Methods
             this.height = height;
             this.width = width;
         }
-
-        public void SetBodrer(char symbol)
-        {
-            //Console.WriteLine(" Выберите символ: '*', '+', '.' " );
-
-            switch (symbol)
+        
+        public void SetBodrer(char newBorder)
+        {            
+            while (true)
             {
-                case '*':
-                    this.border = '*';
-                    break;
-                case '+':
-                    this.border = '+';
-                    break;
-                case'.':
-                    this.border = '.';
-                    break;
-                default:
-                    Console.WriteLine(" Неизвестный символ. ");
-                    break;
+                switch (newBorder)
+                {
+                    case '*':
+                        border = '*';
+                        return;
+                    case '+':
+                        border = '+';
+                        return;
+                    case '.':
+                        border = '.';
+                        return;
+                    default:
+                        Console.WriteLine(" Неизвестный символ! Попробуйте ещё раз.");
+                        newBorder = char.Parse(Console.ReadLine());
+                        break; 
+                }      
             }
+        }
+        public void SetMessage(string message)
+        {
+            this.message = message;
         }
 
         //2.  Implement public Draw() method
@@ -70,50 +73,95 @@ namespace Hello_Cons_Dr_Methods
 
         public void Draw()
         {
-            for (int i = 1; i <= height + positionY; i++)
+            Console.SetCursorPosition(positionX, positionY);
+            Console.Write(border);
+            for (int i = 0; i < width; i++)
             {
-                for (int j = 1; j <= width + positionX; j++)
+                Console.Write(border);
+            }
+            Console.Write(border);
+            Console.WriteLine();
+
+            for (int i = 0; i < height; i++)
+            {
+                Console.SetCursorPosition(positionX, positionY + i + 1);
+                Console.Write(border);
+                for (int j = 0; j < width; j++)
                 {
-                    if (i > positionY)
-                    {
-                        if (j > positionX)
-                        {
-                            if ((i == positionY + 1) || (i == height + positionY))
-                            {
-
-                                //Console.Write(dies);
-                            }
-                            else
-                            {
-                                if ((j == positionX + 1) || (j == width + positionX))
-                                {
-
-                                    //Console.Write(dies);
-                                }
-                                else
-                                {
-                                    //Console.Write(space);
-                                }
-                            }
-
-                        }
-                        else
-                        {
-                            //Console.Write(space);
-                        }
-
-                    }
-
+                    Console.Write(space);
                 }
+                Console.Write(border);
                 Console.WriteLine();
             }
+
+            Console.SetCursorPosition(positionX, positionY + height + 1);
+            Console.Write(border);
+            for (int i = 0; i < width; i++)
+            {
+                Console.Write(border);
+            }
+            Console.Write(border);
+            Console.WriteLine();
+
+            Console.SetCursorPosition(positionX + 1, positionY + height / 2);
+            Console.Write(message);
+            Console.WriteLine(space);
         }
+
+        //public void Draw()
+        //{
+        //    Console.SetCursorPosition(30,10);
+        //    for (int i = 1; i <= height + positionY; i++)
+        //    {
+        //        //Console.SetCursorPosition(30, 10);
+        //        for (int j = 1; j <= width + positionX; j++)
+        //        {
+        //            if (i > positionY)
+        //            {
+        //                if (j > positionX)
+        //                {
+        //                    if ((i == positionY + 1) || (i == height + positionY))
+        //                    {
+        //                        Console.Write(border);
+        //                    }
+        //                    else
+        //                    {
+        //                        if ((j == positionX + 1) || (j == width + positionX))
+        //                        {
+        //                            Console.Write(border);
+        //                        }
+        //                        else
+        //                        {
+        //                            if ((i == positionY + 2) && (j == positionX + 2))
+        //                            {
+        //                                Console.Write(message);
+        //                                j += message.Length - 1;
+        //                            }
+        //                            else
+        //                            {
+        //                                Console.Write(space);
+        //                            }
+        //                        }
+        //                    }
+        //                }
+        //                else
+        //                {
+        //                    Console.Write(space);
+        //                }
+        //            }
+        //        }
+        //        Console.WriteLine();
+        //    }
+        //}
 
         //3.  Implement private method draw() with parameters 
         //for start position, width and height, symbol, message
         //Change the message in the method to return the Box square
+
         //Use Console.SetCursorPosition() method
+
         //Trim the message if necessary
+
 
 
     }
--- 2.2/Hello_Cons_Dr_Methods/Hello_Cons_Dr_Methods/Program.cs
+++ 2.2/Hello_Cons_Dr_Methods/Hello_Cons_Dr_Methods/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,27 +9,46 @@ namespace Hello_Cons_Dr_Methods
 {
     class Program
     {
+        
+    
+    
         static void Main(string[] args)
         {
             try
             {
-                //Implement start position, width and height, symbol, message input
+            //Implement start position, width and height, symbol, message input 
+                
+                Box box = new Box();
+                
+                Console.SetCursorPosition(0, 0);
+
+                Console.Write(" Введите координаты X и Y: ");
+                box.StartPosition(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()));
+
+                Console.Write(" Введите высоту и ширину: ");
+                box.Size(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()));
 
                 
+                Console.WriteLine(" Выберите символ: '*', '+', '.' ");
+                char symbol = char.Parse(Console.ReadLine());
+                box.SetBodrer(symbol);
 
-                
-               
+                Console.Write(" Введите сообщение: ");
+                box.message = Console.ReadLine();
+                Console.WriteLine();
 
-                
+                box.Draw();
+                Console.ReadKey();
 
-               
-                                               
                 //Create Box class instance
 
-                //Use  Box.Draw() method
 
+                //Use  Box.Draw() method   
+
+
+                Console.SetCursorPosition(0, 8);
                 Console.WriteLine("Press any key...");
-            Console.ReadLine();
+                Console.ReadLine();
             }
             catch (Exception)
             {
